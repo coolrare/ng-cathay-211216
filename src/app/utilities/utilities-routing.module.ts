19550d125825683fcb811018d543c61f25be77fb
Will <doggy.huang@gmail.com>
2021-12-16 14:32:51 +0800

    完成 Utilities 模組與 /utilities/animation 元件開發 (啟用延遲載入路由)

--- src/app/utilities/utilities-routing.module.ts
+++ src/app/utilities/utilities-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimationComponent } from './animation/animation.component';
 import { UtilitiesComponent } from './utilities.component';
 
-const routes: Routes = [{ path: '', component: UtilitiesComponent }];
+const routes: Routes = [
+  { path: '', component: UtilitiesComponent },
+  { path: 'animation', component: AnimationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/utilities/utilities.module.ts
+++ src/app/utilities/utilities.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UtilitiesRoutingModule } from './utilities-routing.module';
 import { UtilitiesComponent } from './utilities.component';
+import { AnimationComponent } from './animation/animation.component';
 
 
 @NgModule({
   declarations: [
-    UtilitiesComponent
+    UtilitiesComponent,
+    AnimationComponent
   ],
   imports: [
     CommonModule,
