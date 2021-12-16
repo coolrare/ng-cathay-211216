75b58bed66cb0737b0fbc8b566b18d2b59bd28b8
Will <doggy.huang@gmail.com>
2021-12-16 13:56:17 +0800

    示範各種路由（預設路由、轉向路由、萬用路由）

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
+  // { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
